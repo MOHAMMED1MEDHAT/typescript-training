dff801b99854a64aec57cabf6a1f8a5cc30af262
mohammed medhat <mohamed.medhat2121@gmail.com>
2023-10-23 23:39:24 +0300

    Adding features for lesson 11

---
+++ public/app.js
@@ -0,0 +1,5 @@
+"use strict";
+//we use as to apply a certain interface to the type;
+const newItemFrom = document.querySelector('.new-item-from');
+//inputs
+const type = document.querySelector('#type');
---
+++ src/app.ts
@@ -0,0 +1,5 @@
+//we use as to apply a certain interface to the type;
+const newItemFrom = document.querySelector('.new-item-from') as HTMLFormElement;
+
+//inputs
+const type = document.querySelector('#type') as HTMLSelectElement;
