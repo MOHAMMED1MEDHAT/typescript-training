735f01b42aedfc7203bbd108b68558ea53976460
mohammed medhat <mohamed.medhat2121@gmail.com>
2023-10-24 10:01:47 +0300

    Adding features of lesson 13

--- public/app.js
+++ public/app.js
@@ -1,5 +1,24 @@
 "use strict";
+class Invoice {
+    constructor(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
+    }
+    fromat() {
+        return `owner: ${this.client} details:${this.details} amount: ${this.amount}`;
+    }
+}
 //we use as to apply a certain interface to the type;
 const newItemFrom = document.querySelector('.new-item-from');
 //inputs
 const type = document.querySelector('#type');
+const toFrom = document.querySelector('#toFrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+newItemFrom.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
+});
+const invOne = new Invoice(toFrom.value, details.value, amount.valueAsNumber);
+console.log(invOne.fromat());
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,30 @@
+class Invoice {
+  constructor(
+    readonly client: string,
+    private details: string,
+    public amount: number
+  ) {}
+
+  fromat(): string {
+    return `owner: ${this.client} details:${this.details} amount: ${this.amount}`;
+  }
+}
+
 //we use as to apply a certain interface to the type;
 const newItemFrom = document.querySelector('.new-item-from') as HTMLFormElement;
 
 //inputs
 const type = document.querySelector('#type') as HTMLSelectElement;
+const toFrom = document.querySelector('#toFrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+newItemFrom.addEventListener('submit', (e: Event) => {
+  e.preventDefault();
+
+  console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
+});
+
+const invOne = new Invoice(toFrom.value, details.value, amount.valueAsNumber);
+
+console.log(invOne.fromat());
